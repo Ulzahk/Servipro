76c7d7f6178fd16ddc41b4c302a8ea2eb9c5f4ee
twotorches <54070107+twotorches@users.noreply.github.com>
2019-10-08 17:57:37 -0500

    otros cambios

--- src/main/java/RolesServiceImpl.java
+++ src/main/java/RolesServiceImpl.java
@@ -1,3 +1,9 @@
+
+import java.util.List;
+import model.Person;
+import org.springframework.stereotype.Service;
+import service.RolesService;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +14,14 @@
  *
  * @author PRACTICANTE
  */
-public class RolesServiceImpl {
+@Service("rolesService")
+public class RolesServiceImpl implements RolesService {
+    
+    @Override
+    public List<Person> getListPeople(){
+        return null;
+    }
+    
+    
     
 }
