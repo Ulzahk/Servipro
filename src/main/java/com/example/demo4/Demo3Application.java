e0f5d60a8f32ea87d6b97e38cdfabcc62411643f
CristianFnf <52043363+CristianFnf@users.noreply.github.com>
2019-10-25 09:57:19 -0500

    Crud centro de costos
    CRUD Centro de costos
    
    Co-Authored-By: Alejandro Jose Acosta Fernandez <multimatex@users.noreply.github.com>

--- src/main/java/com/example/demo4/Demo3Application.java
+++ src/main/java/com/example/demo4/Demo3Application.java
@@ -3,9 +3,11 @@ package com.example.demo4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration 
+@EnableAutoConfiguration
+@ComponentScan({"com.example.demo4", "com.example.demo4.model"})
 public class Demo3Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo4/services/api/CentroDeCostosServiceAPI.java
@@ -0,0 +1,10 @@
+
+package com.example.demo4.services.api;
+
+import com.example.demo4.commons.GenericServiceAPI;
+import com.example.demo4.entity.CentroDeCostosEntity;
+
+
+public interface CentroDeCostosServiceAPI extends GenericServiceAPI<CentroDeCostosEntity, Long>{
+    
+}
