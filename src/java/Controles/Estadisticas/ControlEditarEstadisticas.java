c1f3c492c712229f8d0631d1f17df48ada85fa0d
Ulzahk <46821988+Ulzahk@users.noreply.github.com>
2019-11-19 10:38:41 -0500

    Cambios Recientes a la Vista Estadisticas
    1. Creación de filtros Mesualidad, Quincena 1 y Quincena 2
    2. Creación de tabla estadistica con campos interactivos y alias individuales.

---
+++ src/java/Controles/Estadisticas/ControlEditarEstadisticas.java
@@ -0,0 +1,132 @@
+
+package Controles.Estadisticas;
+
+import Modelos.Conectar;
+import Modelos.Estadisticas.Estadisticas;
+import Modelos.Estadisticas.EstadisticasValidacion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("estadisticas.htm")
+public class ControlEditarEstadisticas {
+    
+    EstadisticasValidacion estadisticasValidacion;
+    private JdbcTemplate jdbcTemplate;
+    
+    public ControlEditarEstadisticas(){
+        this.estadisticasValidacion= new EstadisticasValidacion();
+        Conectar con = new Conectar();
+        this.jdbcTemplate = new JdbcTemplate(con.conectar());
+    }
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public ModelAndView estadisticas(HttpServletRequest request){
+        
+        ModelAndView mav=new ModelAndView();
+        int id_estadistica=Integer.parseInt(request.getParameter("id_estadistica"));
+        Estadisticas datos=this.selectEstadisticas(id_estadistica);
+        mav.setViewName("estadisticas");
+        mav.addObject("estadisticas", new Estadisticas(id_estadistica, datos.getId_empleado(),
+        datos.getId_dia1(), datos.getId_dia2(), datos.getId_dia3(), datos.getId_dia4(), datos.getId_dia5(),
+        datos.getId_dia6(), datos.getId_dia7(), datos.getId_dia8(), datos.getId_dia9(), datos.getId_dia10(),
+        datos.getId_dia11(), datos.getId_dia12(), datos.getId_dia13(), datos.getId_dia14(), datos.getId_dia15(),
+        datos.getId_dia16(), datos.getId_dia17(), datos.getId_dia18(), datos.getId_dia19(), datos.getId_dia20(),
+        datos.getId_dia21(), datos.getId_dia22(), datos.getId_dia23(), datos.getId_dia24(), datos.getId_dia25(),
+        datos.getId_dia26(), datos.getId_dia27(), datos.getId_dia28(), datos.getId_dia29(), datos.getId_dia30()));
+        return mav;
+    }
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public ModelAndView estadisticas(@ModelAttribute("estadisticas") Estadisticas est,
+    BindingResult result, SessionStatus status, HttpServletRequest request){
+        
+        this.estadisticasValidacion.validate(est, result);
+        if(result.hasErrors()){
+            
+            ModelAndView mav=new ModelAndView();
+            int id_estadistica=Integer.parseInt(request.getParameter("id_estadistica"));
+            Estadisticas datos=this.selectEstadisticas(id_estadistica);
+            mav.setViewName("estadisticas");
+            mav.addObject("estadisticas", new Estadisticas(id_estadistica, datos.getId_empleado(),
+            datos.getId_dia1(), datos.getId_dia2(), datos.getId_dia3(), datos.getId_dia4(), datos.getId_dia5(),
+            datos.getId_dia6(), datos.getId_dia7(), datos.getId_dia8(), datos.getId_dia9(), datos.getId_dia10(),
+            datos.getId_dia11(), datos.getId_dia12(), datos.getId_dia13(), datos.getId_dia14(), datos.getId_dia15(),
+            datos.getId_dia16(), datos.getId_dia17(), datos.getId_dia18(), datos.getId_dia19(), datos.getId_dia20(),
+            datos.getId_dia21(), datos.getId_dia22(), datos.getId_dia23(), datos.getId_dia24(), datos.getId_dia25(),
+            datos.getId_dia26(), datos.getId_dia27(), datos.getId_dia28(), datos.getId_dia29(), datos.getId_dia30()));
+            return mav;
+            
+        }else{
+            
+            int id_estadistica=Integer.parseInt(request.getParameter("id_estadistica"));
+            this.jdbcTemplate.update("update nm_estadistica set id_empleado=?, id_dia1=?, id_dia2=?, "
+                    + "id_dia3=?, id_dia4=?, id_dia5=?, id_dia6=?, id_dia7=?, id_dia8=?, id_dia9=?, "
+                    + "id_dia10=?, id_dia11=?, id_dia12=?, id_dia13=?, id_dia14=?, id_dia15=?, id_dia16=?, "
+                    + "id_dia17=?, id_dia18=?, id_dia19=?, id_dia20=?, id_dia21=?, id_dia22=?, id_dia22=?, "
+                    + "id_dia23=?, id_dia24=?, id_dia25=?, id_dia26=?, id_dia27=?, id_dia28=?, id_dia29=?, "
+                    + "id_dia30=? where id_estadistica=?", est.getId_empleado(), est.getId_dia1(), est.getId_dia2(), 
+                    est.getId_dia3(), est.getId_dia4(), est.getId_dia5(), est.getId_dia6(), est.getId_dia7(), est.getId_dia8(), 
+                    est.getId_dia9(), est.getId_dia10(), est.getId_dia11(), est.getId_dia12(), est.getId_dia13(), 
+                    est.getId_dia14(), est.getId_dia15(), est.getId_dia16(), est.getId_dia17(), est.getId_dia18(), 
+                    est.getId_dia19(), est.getId_dia20(), est.getId_dia21(), est.getId_dia22(), est.getId_dia23(), 
+                    est.getId_dia24(), est.getId_dia25(), est.getId_dia26(), est.getId_dia27(), est.getId_dia28(),
+                    est.getId_dia29(), est.getId_dia30(), id_estadistica);
+            return new ModelAndView("redirect:/estadisticas.htm");
+        }
+    }
+    
+    public Estadisticas selectEstadisticas (int id_estadistica){
+        final Estadisticas est = new Estadisticas();
+        String quer="select * from nm_estadisticas where id_estadistica='"+id_estadistica+"'";
+        return (Estadisticas) jdbcTemplate.query(quer, new ResultSetExtractor<Estadisticas>(){
+            public Estadisticas extractData(ResultSet rs)throws SQLException, DataAccessException{
+                if(rs.next()){
+                    est.setId_empleado(rs.getInt("id_empleado"));
+                    est.setId_dia1(rs.getInt("id_dia1"));
+                    est.setId_dia2(rs.getInt("id_dia2"));
+                    est.setId_dia3(rs.getInt("id_dia3"));
+                    est.setId_dia4(rs.getInt("id_dia4"));
+                    est.setId_dia5(rs.getInt("id_dia5"));
+                    est.setId_dia6(rs.getInt("id_dia6"));
+                    est.setId_dia7(rs.getInt("id_dia7"));
+                    est.setId_dia8(rs.getInt("id_dia8"));
+                    est.setId_dia9(rs.getInt("id_dia9"));
+                    est.setId_dia10(rs.getInt("id_dia10"));
+                    est.setId_dia11(rs.getInt("id_dia11"));
+                    est.setId_dia2(rs.getInt("id_dia12"));
+                    est.setId_dia3(rs.getInt("id_dia13"));
+                    est.setId_dia4(rs.getInt("id_dia14"));
+                    est.setId_dia5(rs.getInt("id_dia15"));
+                    est.setId_dia6(rs.getInt("id_dia16"));
+                    est.setId_dia7(rs.getInt("id_dia17"));
+                    est.setId_dia8(rs.getInt("id_dia18"));
+                    est.setId_dia9(rs.getInt("id_dia19"));
+                    est.setId_dia10(rs.getInt("id_dia20"));
+                    est.setId_dia11(rs.getInt("id_dia21"));
+                    est.setId_dia2(rs.getInt("id_dia22"));
+                    est.setId_dia3(rs.getInt("id_dia23"));
+                    est.setId_dia4(rs.getInt("id_dia24"));
+                    est.setId_dia5(rs.getInt("id_dia25"));
+                    est.setId_dia6(rs.getInt("id_dia26"));
+                    est.setId_dia7(rs.getInt("id_dia27"));
+                    est.setId_dia8(rs.getInt("id_dia28"));
+                    est.setId_dia9(rs.getInt("id_dia29"));
+                    est.setId_dia10(rs.getInt("id_dia30"));
+                }
+                return est;
+            }
+        });
+    }
+}
---
+++ src/java/Modelos/Estadisticas/Estadisticas.java
@@ -0,0 +1,371 @@
+
+package Modelos.Estadisticas;
+
+
+public class Estadisticas {
+    
+    private int id_estadistica;
+    private int id_empleado;
+    private int id_dia1;
+    private int id_dia2;
+    private int id_dia3;
+    private int id_dia4;
+    private int id_dia5;
+    private int id_dia6;
+    private int id_dia7;
+    private int id_dia8;
+    private int id_dia9;
+    private int id_dia10;
+    private int id_dia11;
+    private int id_dia12;
+    private int id_dia13;
+    private int id_dia14;
+    private int id_dia15;
+    private int id_dia16;
+    private int id_dia17;
+    private int id_dia18;
+    private int id_dia19;
+    private int id_dia20;
+    private int id_dia21;
+    private int id_dia22;
+    private int id_dia23;
+    private int id_dia24;
+    private int id_dia25;
+    private int id_dia26;
+    private int id_dia27;
+    private int id_dia28;
+    private int id_dia29;
+    private int id_dia30;
+
+    public Estadisticas() {
+    }
+
+    public Estadisticas(int id_empleado, int id_dia1, int id_dia2, int id_dia3, int id_dia4, int id_dia5, int id_dia6, int id_dia7, int id_dia8, int id_dia9, int id_dia10, int id_dia11, int id_dia12, int id_dia13, int id_dia14, int id_dia15, int id_dia16, int id_dia17, int id_dia18, int id_dia19, int id_dia20, int id_dia21, int id_dia22, int id_dia23, int id_dia24, int id_dia25, int id_dia26, int id_dia27, int id_dia28, int id_dia29, int id_dia30) {
+        this.id_empleado = id_empleado;
+        this.id_dia1 = id_dia1;
+        this.id_dia2 = id_dia2;
+        this.id_dia3 = id_dia3;
+        this.id_dia4 = id_dia4;
+        this.id_dia5 = id_dia5;
+        this.id_dia6 = id_dia6;
+        this.id_dia7 = id_dia7;
+        this.id_dia8 = id_dia8;
+        this.id_dia9 = id_dia9;
+        this.id_dia10 = id_dia10;
+        this.id_dia11 = id_dia11;
+        this.id_dia12 = id_dia12;
+        this.id_dia13 = id_dia13;
+        this.id_dia14 = id_dia14;
+        this.id_dia15 = id_dia15;
+        this.id_dia16 = id_dia16;
+        this.id_dia17 = id_dia17;
+        this.id_dia18 = id_dia18;
+        this.id_dia19 = id_dia19;
+        this.id_dia20 = id_dia20;
+        this.id_dia21 = id_dia21;
+        this.id_dia22 = id_dia22;
+        this.id_dia23 = id_dia23;
+        this.id_dia24 = id_dia24;
+        this.id_dia25 = id_dia25;
+        this.id_dia26 = id_dia26;
+        this.id_dia27 = id_dia27;
+        this.id_dia28 = id_dia28;
+        this.id_dia29 = id_dia29;
+        this.id_dia30 = id_dia30;
+    }
+
+    public Estadisticas(int id_estadistica, int id_empleado, int id_dia1, int id_dia2, int id_dia3, int id_dia4, int id_dia5, int id_dia6, int id_dia7, int id_dia8, int id_dia9, int id_dia10, int id_dia11, int id_dia12, int id_dia13, int id_dia14, int id_dia15, int id_dia16, int id_dia17, int id_dia18, int id_dia19, int id_dia20, int id_dia21, int id_dia22, int id_dia23, int id_dia24, int id_dia25, int id_dia26, int id_dia27, int id_dia28, int id_dia29, int id_dia30) {
+        this.id_estadistica = id_estadistica;
+        this.id_empleado = id_empleado;
+        this.id_dia1 = id_dia1;
+        this.id_dia2 = id_dia2;
+        this.id_dia3 = id_dia3;
+        this.id_dia4 = id_dia4;
+        this.id_dia5 = id_dia5;
+        this.id_dia6 = id_dia6;
+        this.id_dia7 = id_dia7;
+        this.id_dia8 = id_dia8;
+        this.id_dia9 = id_dia9;
+        this.id_dia10 = id_dia10;
+        this.id_dia11 = id_dia11;
+        this.id_dia12 = id_dia12;
+        this.id_dia13 = id_dia13;
+        this.id_dia14 = id_dia14;
+        this.id_dia15 = id_dia15;
+        this.id_dia16 = id_dia16;
+        this.id_dia17 = id_dia17;
+        this.id_dia18 = id_dia18;
+        this.id_dia19 = id_dia19;
+        this.id_dia20 = id_dia20;
+        this.id_dia21 = id_dia21;
+        this.id_dia22 = id_dia22;
+        this.id_dia23 = id_dia23;
+        this.id_dia24 = id_dia24;
+        this.id_dia25 = id_dia25;
+        this.id_dia26 = id_dia26;
+        this.id_dia27 = id_dia27;
+        this.id_dia28 = id_dia28;
+        this.id_dia29 = id_dia29;
+        this.id_dia30 = id_dia30;
+    }
+
+    public int getId_estadistica() {
+        return id_estadistica;
+    }
+
+    public void setId_estadistica(int id_estadistica) {
+        this.id_estadistica = id_estadistica;
+    }
+
+    public int getId_empleado() {
+        return id_empleado;
+    }
+
+    public void setId_empleado(int id_empleado) {
+        this.id_empleado = id_empleado;
+    }
+
+    public int getId_dia1() {
+        return id_dia1;
+    }
+
+    public void setId_dia1(int id_dia1) {
+        this.id_dia1 = id_dia1;
+    }
+
+    public int getId_dia2() {
+        return id_dia2;
+    }
+
+    public void setId_dia2(int id_dia2) {
+        this.id_dia2 = id_dia2;
+    }
+
+    public int getId_dia3() {
+        return id_dia3;
+    }
+
+    public void setId_dia3(int id_dia3) {
+        this.id_dia3 = id_dia3;
+    }
+
+    public int getId_dia4() {
+        return id_dia4;
+    }
+
+    public void setId_dia4(int id_dia4) {
+        this.id_dia4 = id_dia4;
+    }
+
+    public int getId_dia5() {
+        return id_dia5;
+    }
+
+    public void setId_dia5(int id_dia5) {
+        this.id_dia5 = id_dia5;
+    }
+
+    public int getId_dia6() {
+        return id_dia6;
+    }
+
+    public void setId_dia6(int id_dia6) {
+        this.id_dia6 = id_dia6;
+    }
+
+    public int getId_dia7() {
+        return id_dia7;
+    }
+
+    public void setId_dia7(int id_dia7) {
+        this.id_dia7 = id_dia7;
+    }
+
+    public int getId_dia8() {
+        return id_dia8;
+    }
+
+    public void setId_dia8(int id_dia8) {
+        this.id_dia8 = id_dia8;
+    }
+
+    public int getId_dia9() {
+        return id_dia9;
+    }
+
+    public void setId_dia9(int id_dia9) {
+        this.id_dia9 = id_dia9;
+    }
+
+    public int getId_dia10() {
+        return id_dia10;
+    }
+
+    public void setId_dia10(int id_dia10) {
+        this.id_dia10 = id_dia10;
+    }
+
+    public int getId_dia11() {
+        return id_dia11;
+    }
+
+    public void setId_dia11(int id_dia11) {
+        this.id_dia11 = id_dia11;
+    }
+
+    public int getId_dia12() {
+        return id_dia12;
+    }
+
+    public void setId_dia12(int id_dia12) {
+        this.id_dia12 = id_dia12;
+    }
+
+    public int getId_dia13() {
+        return id_dia13;
+    }
+
+    public void setId_dia13(int id_dia13) {
+        this.id_dia13 = id_dia13;
+    }
+
+    public int getId_dia14() {
+        return id_dia14;
+    }
+
+    public void setId_dia14(int id_dia14) {
+        this.id_dia14 = id_dia14;
+    }
+
+    public int getId_dia15() {
+        return id_dia15;
+    }
+
+    public void setId_dia15(int id_dia15) {
+        this.id_dia15 = id_dia15;
+    }
+
+    public int getId_dia16() {
+        return id_dia16;
+    }
+
+    public void setId_dia16(int id_dia16) {
+        this.id_dia16 = id_dia16;
+    }
+
+    public int getId_dia17() {
+        return id_dia17;
+    }
+
+    public void setId_dia17(int id_dia17) {
+        this.id_dia17 = id_dia17;
+    }
+
+    public int getId_dia18() {
+        return id_dia18;
+    }
+
+    public void setId_dia18(int id_dia18) {
+        this.id_dia18 = id_dia18;
+    }
+
+    public int getId_dia19() {
+        return id_dia19;
+    }
+
+    public void setId_dia19(int id_dia19) {
+        this.id_dia19 = id_dia19;
+    }
+
+    public int getId_dia20() {
+        return id_dia20;
+    }
+
+    public void setId_dia20(int id_dia20) {
+        this.id_dia20 = id_dia20;
+    }
+
+    public int getId_dia21() {
+        return id_dia21;
+    }
+
+    public void setId_dia21(int id_dia21) {
+        this.id_dia21 = id_dia21;
+    }
+
+    public int getId_dia22() {
+        return id_dia22;
+    }
+
+    public void setId_dia22(int id_dia22) {
+        this.id_dia22 = id_dia22;
+    }
+
+    public int getId_dia23() {
+        return id_dia23;
+    }
+
+    public void setId_dia23(int id_dia23) {
+        this.id_dia23 = id_dia23;
+    }
+
+    public int getId_dia24() {
+        return id_dia24;
+    }
+
+    public void setId_dia24(int id_dia24) {
+        this.id_dia24 = id_dia24;
+    }
+
+    public int getId_dia25() {
+        return id_dia25;
+    }
+
+    public void setId_dia25(int id_dia25) {
+        this.id_dia25 = id_dia25;
+    }
+
+    public int getId_dia26() {
+        return id_dia26;
+    }
+
+    public void setId_dia26(int id_dia26) {
+        this.id_dia26 = id_dia26;
+    }
+
+    public int getId_dia27() {
+        return id_dia27;
+    }
+
+    public void setId_dia27(int id_dia27) {
+        this.id_dia27 = id_dia27;
+    }
+
+    public int getId_dia28() {
+        return id_dia28;
+    }
+
+    public void setId_dia28(int id_dia28) {
+        this.id_dia28 = id_dia28;
+    }
+
+    public int getId_dia29() {
+        return id_dia29;
+    }
+
+    public void setId_dia29(int id_dia29) {
+        this.id_dia29 = id_dia29;
+    }
+
+    public int getId_dia30() {
+        return id_dia30;
+    }
+
+    public void setId_dia30(int id_dia30) {
+        this.id_dia30 = id_dia30;
+    }
+    
+    
+    
+    
+}
